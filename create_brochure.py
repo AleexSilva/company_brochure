6a894711d0f1019512287f22ec02de29f8773a11
AleexSilva <silva.alexis94@gmail.com>
2025-02-28 21:11:29 -0300

    [PROD] New feature

--- create_brochure.py
+++ create_brochure.py
@@ -6,6 +6,7 @@ from dotenv import load_dotenv
 from bs4 import BeautifulSoup
 from IPython.display import Markdown, display, update_display
 from openai import OpenAI
+from logger import logger
 
 # Initialize and constants
 
@@ -15,6 +16,8 @@ api_key = os.getenv("OPENAI_API_KEY")
 
 # check the API key
 if api_key and api_key.startswith('sk-proj-') and len(api_key)>10:
-    print("API key looks good so far")
+    logger.info("API key looks good so far")
 else:
-    print("There might be a problem with your API key? Please visit the troubleshooting notebook!")
+    logger.error("There might be a problem with your API key? Please visit the troubleshooting notebook!")
+
+
