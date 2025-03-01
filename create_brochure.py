d74674575de7d762b5e46e0fa29c75fae5887062
AleexSilva <silva.alexis94@gmail.com>
2025-02-28 21:12:41 -0300

    Create a Website calse

--- create_brochure.py
+++ create_brochure.py
@@ -21,3 +21,34 @@ else:
     logger.error("There might be a problem with your API key? Please visit the troubleshooting notebook!")
 
 
+MODEL = 'gpt-4o-mini'
+openai = OpenAI(api_key = api_key)
+
+# Some websites need you to use proper headers when fetching them:
+headers = {
+ "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36"
+}
+
+# Create a class to represent a webpage
+class Website:
+    
+    url: str
+    title: str
+    body: str
+    links: List[str]
+    text: str
+    
+    def __init__(self, url):
+        self.url = url
+        response = requests.get(url,headers = headers)
+        self.body = response.content
+        soup = BeautifulSoup(self.body, 'html.parser')
+        self.title = soup.title.string if soup.title else "No title Found"
+        if soup.body:
+            for irrelevant in soup.body("script","style","inputs","img"):
+                irrelevant.decompose()
+            self.text = soup.body.get_text(separator="\n",strip=True)
+        else:
+            self.text = ""
+        links = [links.get('href') for links in soup.find_all('a')]
+        self.links = [link for link in links if link]
