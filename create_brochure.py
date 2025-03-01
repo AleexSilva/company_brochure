822f88a7059972a5c8a32ebfb2aa7c823f4571f0
AleexSilva <silva.alexis94@gmail.com>
2025-02-28 21:14:09 -0300

    System prompt

--- create_brochure.py
+++ create_brochure.py
@@ -54,3 +54,8 @@ class Website:
         self.links = [link for link in links if link]
     def get_contents(self):
         return f"Webpage Title:\n{self.title}\nWebpage Contents:\n{self.text}\n\n"        
+
+# Sytem Prompt contruction
+link_system_prompt = "You are provided with a list of links found on a webpage. \
+You are able to decide which of the links would be most relevant to include in a brochure about the company, \
+such as links to an About page, or a Company page, or Careers/Jobs pages.\n"
