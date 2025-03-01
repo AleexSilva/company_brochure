06be7b0fd8c5c2b3f043ba8be64d1f8f75d83a70
AleexSilva <silva.alexis94@gmail.com>
2025-03-01 08:48:35 -0300

    [LLM] User Prompt

--- create_brochure.py
+++ create_brochure.py
@@ -68,3 +68,12 @@ link_system_prompt += """
     ]
 }
 """
+
+# User Prompt
+def get_links_user_prompt(website):
+    user_prompt = f"Here is the list of links on the website of {website.url} - "
+    user_prompt += "please decide which of these are relevant web links for a brochure about the company, respond with the full https URL in JSON format. \
+Do not include Terms of Service, Privacy, email links.\n"
+    user_prompt += "Links (some might be relative links):\n"
+    user_prompt += "\n".join(website.links)
+    return user_prompt
